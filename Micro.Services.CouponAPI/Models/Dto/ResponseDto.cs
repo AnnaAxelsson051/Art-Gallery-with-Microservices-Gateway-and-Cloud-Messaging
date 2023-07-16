5f68d4329852675525cbeac74306a0305cd71ede
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-16 23:11:08 +0200

    Add responseDto class

---
+++ Micro.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Micro.Services.CouponAPI.Models.Dto
+{
+	public class ResponseDto
+	{
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } 
+        public string Message { get; set; } 
+    }
+}
+
