de1f4b0c6fd9bd304892a2a5f80374442ed12c7d
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-16 21:48:16 +0200

    Configure db connection

--- Micro.Services.CouponAPI/Program.cs
+++ Micro.Services.CouponAPI/Program.cs
@@ -1,6 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using AutoMapper;
+using Micro.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
