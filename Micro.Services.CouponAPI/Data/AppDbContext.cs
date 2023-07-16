54677f72636189b7d4e57b0f498a44658d95ba4f
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-16 21:40:49 +0200

    Add db context

---
+++ Micro.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Micro.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micro.Services.CouponAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
+
--- Micro.Services.CouponAPI/Models/Coupon.cs
+++ Micro.Services.CouponAPI/Models/Coupon.cs
@@ -5,9 +5,11 @@ namespace Micro.Services.CouponAPI.Models
 {
 	public class Coupon
 	{
-      
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
