b6200fcb640bf3d0156a3ec1c46122c40d3f663a
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-16 23:01:43 +0200

    Add couponcontroller and endpoints for getting coupons

---
+++ Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Micro.Services.CouponAPI.Data;
+using Micro.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.Services.CouponAPI.Controllers
+{
+    [Route("api/coupon")]
+    [ApiController]
+    public class CouponAPIController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u => u.CouponId == id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
+
--- Micro.Services.CouponAPI/Program.cs
+++ Micro.Services.CouponAPI/Program.cs
@@ -29,6 +29,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+//ApplyMigration();
 
 app.Run();
+
+//Auto apply pending migrations to db 
+/*void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if(_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+*/
 
