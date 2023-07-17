e6b0b480b015f595833c8ad257b66415db5175fa
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-17 15:18:18 +0200

    Add CouponService

---
+++ Micro.Web/Service/CouponService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Micro.Web.Models;
+using Micro.Web.Service.IService;
+
+namespace Micro.Web.Service
+{
+	public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- Micro.Web/Utility/SD.cs
+++ Micro.Web/Utility/SD.cs
@@ -3,6 +3,7 @@ namespace Micro.Web.Utility
 {
     public class SD
     {
+        public static string CouponAPIBase { get; set; }
         public enum ApiType
         {
             GET,
