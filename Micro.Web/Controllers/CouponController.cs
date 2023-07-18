5d91a755fff6a4b1fd78840cc552b5c9dd04b271
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-18 23:28:11 +0200

    Add coupon index view

---
+++ Micro.Web/Controllers/CouponController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Micro.Web.Models;
+using Micro.Web.Service;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Micro.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        //Displaying all the coupons
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+          
+            return View(list);
+        }
+    }
+}
+
