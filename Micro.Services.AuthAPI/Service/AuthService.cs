484946a3562908def1861767180ccc694dcabb73
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-20 13:23:26 +0200

    Add IAuthService

---
+++ Micro.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Micro.Services.AuthAPI.Data;
+using Micro.Services.AuthAPI.Models;
+using Micro.Services.AuthAPI.Models.Dto;
+using Micro.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Micro.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db,
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Micro.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Micro.Services.AuthAPI.Models.Dto;
+
+namespace Micro.Services.AuthAPI.Service.IService
+{
+	public interface IAuthService
+	{
+
+		Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+		Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+	}
+}
+
