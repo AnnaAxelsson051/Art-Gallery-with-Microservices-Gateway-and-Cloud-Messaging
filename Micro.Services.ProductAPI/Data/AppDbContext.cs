f4f109a5b118440c3d191852f2d17cbe5cd5085c
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-26 13:54:08 +0200

    Add product models and product seed data

---
+++ Micro.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Micro.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micro.Services.ProductAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 1,
+                Name = "Golden flower",
+                Price = 150,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/603x403",
+                CategoryName = "Abstract"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 2,
+                Name = "Eiffel tower",
+                Price = 130.99,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/602x402",
+                CategoryName = "Cityscape"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 3,
+                Name = "Wild river",
+                Price = 100.99,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/601x401",
+                CategoryName = "Landscape"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductId = 4,
+                Name = "Dalai Lama",
+                Price = 150,
+                Description = " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.",
+                ImageUrl = "https://placehold.co/600x400",
+                CategoryName = "Portrait"
+            });
+        }
+    }
+}
