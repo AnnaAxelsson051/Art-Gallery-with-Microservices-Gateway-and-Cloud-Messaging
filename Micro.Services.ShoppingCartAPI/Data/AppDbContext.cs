d5b5000ac04c225605fafbeaf06626c2079b505c
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-27 17:13:11 +0200

    Add appdb context, extensions and program content to shoppingcartAPI

---
+++ Micro.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Micro.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micro.Services.ShoppingCartAPI.Data
+{
+	public class AppDbContext : DbContext
+	{
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+
+    }
+}
---
+++ Micro.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using Micro.Services.ProductAPI;
+using Micro.Services.ProductAPI.Models;
+using Micro.Services.ProductAPI.Models.Dto;
+
+namespace Micro.Services.ProductAPI
+{
+    //Mapping between Coupon and Dto
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap(); ;
+            
+            });
+            return mappingConfig;
+        }
+    }
+}
