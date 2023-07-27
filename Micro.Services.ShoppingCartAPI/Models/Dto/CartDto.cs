6f4c9ae67f8cd8b32adafcd1cbaf4398ee107209
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-27 16:59:03 +0200

    Add response and cart dto

---
+++ Micro.Services.ShoppingCartAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Micro.Services.ShoppingCartAPI.Models.Dto
+{
+	public class CartDto
+	{
+		public CartHeaderDto CartHeader { get; set; }
+		public IEnumerable <CartDetailsDto>? CartDetails { get; set; }
+	}
+}
+
---
+++ Micro.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Micro.Services.ShoppingCartAPI.Models.Dto
+{
+	public class CouponDto
+	{
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
+
