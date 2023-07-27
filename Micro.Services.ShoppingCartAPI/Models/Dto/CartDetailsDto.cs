6710675a07b49af5478352535d1788c4566df885
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-27 16:48:23 +0200

    Add cart details dto

---
+++ Micro.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micro.Services.ShoppingCartAPI.Models.Dto
+{
+	public class CartDetailsDto
+	{
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDto? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
+
