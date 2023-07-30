345c4b88bfe3ff1136f0f91cd7906a88a1ddc240
Anna Axelsson <a.axelsson51@gmail.com>
2023-07-30 15:42:13 +0200

    Add publish message to service bus functionality

--- Micro.MessageBus/MessageBus.cs
+++ Micro.MessageBus/MessageBus.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
 namespace Micro.MessageBus
 {
-	public class MessageBus
-	{
-		public MessageBus()
-		{
-		}
-	}
+    public class MessageBus : IMessageBus
+    {
+        private string connectionString = "Endpoint=sb://microweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=n17/gHik39IGhI7jz8moKbN64YMatVHoR+ASbLKv2gQ=";
+
+        //Publishing message to service bus
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+        }
+    }
 }
 
