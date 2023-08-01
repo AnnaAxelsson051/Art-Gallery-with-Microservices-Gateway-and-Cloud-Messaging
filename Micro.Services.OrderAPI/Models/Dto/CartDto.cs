0eae82374699ae57fba6f36b3753e43afb2823e1
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-01 17:36:26 +0200

    Add OrderAPI project and dtos

---
+++ Micro.Services.OrderAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Micro.Services.OrderAPI.Models.Dto
+{
+	public class CartDto
+	{
+		public CartHeaderDto CartHeader { get; set; }
+		public IEnumerable <CartDetailsDto>? CartDetails { get; set; }
+	}
+}
+
---
+++ Micro.Services.OrderAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micro.Services.OrderAPI.Models.Dto
+{
+	public class CartHeaderDto
+	{
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+    
+        public string? Name { get; set; }
+  
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
+
