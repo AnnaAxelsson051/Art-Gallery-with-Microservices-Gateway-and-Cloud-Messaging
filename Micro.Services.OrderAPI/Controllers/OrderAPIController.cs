766069fa599780c0db49fab5eb7ea368630d882f
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-02 00:26:28 +0200

    Add OrderAPIController

---
+++ Micro.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Micro.MessageBus;
+using Micro.Services.OrderAPI.Data;
+using Micro.Services.OrderAPI.Models.Dto;
+using Micro.Services.ShoppingCartAPI.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Micro.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private IProductService _productService;
+        public OrderAPIController(AppDbContext db,
+            IProductService productService, IMapper mapper)
+        {
+            _db = db;
+            this._response = new ResponseDto();
+            _productService = productService;
+            _mapper = mapper;
+        }
+    }
+}
+
