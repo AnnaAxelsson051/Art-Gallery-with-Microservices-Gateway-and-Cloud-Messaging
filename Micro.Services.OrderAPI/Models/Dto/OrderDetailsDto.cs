8a2647a3f9a8b17d82228975bca9f2a5c1dbfa9a
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-01 18:10:36 +0200

    Add OrderDetails and OrderHeader

---
+++ Micro.Services.OrderAPI/Models/Dto/OrderDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Micro.Services.OrderAPI.Models.Dto
+{
+	public class OrderDetailsDto
+	{
+        [Key]
+        public int OrderDetailsId { get; set; }
+        public int OrderHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
+
---
+++ Micro.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Micro.Services.OrderAPI.Models.Dto;
+
+namespace Micro.Services.OrderAPI.Models
+{
+	public class OrderDetails
+	{
+        [Key]
+        public int OrderDetailsId { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader? OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
+
