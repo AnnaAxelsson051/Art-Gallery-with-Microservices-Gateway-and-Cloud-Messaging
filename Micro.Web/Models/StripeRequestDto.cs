adce98ebc5728a9b43639968863857ed1527188b
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-02 14:15:45 +0200

    Add StripeRequestDto

---
+++ Micro.Web/Models/StripeRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Micro.Web.Models
+{
+	public class StripeRequestDto
+	{
+		public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
+
