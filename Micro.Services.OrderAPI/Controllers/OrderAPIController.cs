db12e857bb4841d5b3ebbcdd78878d044c619775
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-02 18:42:19 +0200

    Modify Stripe coupon functionality

--- Micro.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Micro.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -79,6 +79,14 @@ namespace Micro.Services.OrderAPI.Controllers
 
                 };
 
+                var DiscountsObj = new List<SessionDiscountOptions>()
+                {
+                    new SessionDiscountOptions
+                    {
+                        Coupon=stripeRequestDto.OrderHeader.CouponCode
+                    }
+                };
+
                 foreach (var item in stripeRequestDto.OrderHeader.OrderDetails)
                 {
                     var sessionLineItem = new SessionLineItemOptions
@@ -97,7 +105,10 @@ namespace Micro.Services.OrderAPI.Controllers
 
                     options.LineItems.Add(sessionLineItem);
                 }
-             
+                if (stripeRequestDto.OrderHeader.Discount > 0)
+                {
+                    options.Discounts = DiscountsObj;
+                }
                 var service = new SessionService();
                 Session session = service.Create(options);
                 stripeRequestDto.StripeSessionUrl = session.Url;
