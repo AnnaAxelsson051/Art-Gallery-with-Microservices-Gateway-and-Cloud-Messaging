ad5318d1b77d1b58d64a4383ea7e41d3ca98c32d
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-02 22:46:50 +0200

    Add RewardAPI project plus model class

---
+++ Micro.Services.RewardAPI/Models/Rewards.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Micro.Services.RewardAPI.Models
+{
+	public class Rewards
+	{
+		public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime RewardsDate { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
+
