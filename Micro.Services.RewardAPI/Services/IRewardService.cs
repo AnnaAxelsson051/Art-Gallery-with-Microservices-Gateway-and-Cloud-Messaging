3890cab99ff17d4848ccafe3d2f69538047978eb
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-03 14:29:35 +0200

    Add Reward service

---
+++ Micro.Services.RewardAPI/Services/IRewardService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Micro.Services.RewardAPI.Message;
+
+namespace Micro.Services.RewardAPI.Services
+{
+	public interface IRewardService
+	{
+		Task UpdateRewards(RewardsMessage rewardsMessage);
+     
+    }
+}
+
---
+++ Micro.Services.RewardAPI/Services/RewardService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using Micro.Services.RewardAPI.Data;
+using Micro.Services.RewardAPI.Message;
+using Micro.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micro.Services.RewardAPI.Services
+{
+	public class RewardService : IRewardService
+	{
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public RewardService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            _dbOptions = dbOptions;
+        }
+
+        //Updating the rewards info 
+        public async Task UpdateRewards(RewardsMessage rewardsMessage)
+        {
+            try
+            {
+                Rewards rewards = new()
+                {
+                    OrderId = rewardsMessage.OrderId,
+                    RewardsActivity = rewardsMessage.RewardsActivity,
+                    UserId = rewardsMessage.UserId,
+                    RewardsDate = DateTime.Now
+                };
+                await using var _db = new AppDbContext(_dbOptions);
+                await _db.Rewards.AddAsync(rewards);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
+
