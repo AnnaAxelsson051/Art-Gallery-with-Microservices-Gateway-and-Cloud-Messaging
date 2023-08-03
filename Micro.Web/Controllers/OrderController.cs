8205209a52569bef699efe347b9f0d7f35305d7f
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-03 20:17:36 +0200

    Add pagination, sort and search functionality

---
+++ Micro.Web/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Micro.Web.Models;
+using Micro.Web.Service;
+using Micro.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Micro.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult OrderIndex()
+        {
+            return View();
+        }
+
+        //Fetching order data based on user role
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeaderDto> list;
+            string userId = "";
+            if (!User.IsInRole(SD.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            }
+            ResponseDto response = _orderService.GetAllOrder(userId).GetAwaiter().GetResult();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject <List<OrderHeaderDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+            return Json(new { data = list });
+        }
+    }
+}
+
---
+++ Micro.Web/wwwroot/js/order.js
@@ -0,0 +1,20 @@
+﻿//Retrieving data from the get all endpoint
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: "/order/getall" },
+        "columns": [
+            { data: 'orderHeaderId', "width":"5%"},
+            { data: 'email', "width": "25%" },
+            { data: 'email', "width": "20%" },
+            { data: 'email', "width": "10%" },
+            { data: 'email', "width": "10%" },
+            { data: 'email', "width": "10%" },
+        ]
+    })
+}
