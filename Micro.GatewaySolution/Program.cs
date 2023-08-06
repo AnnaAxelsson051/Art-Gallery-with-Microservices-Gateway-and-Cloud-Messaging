815a51e6ee575574ff273328cde6eca48fe59d7f
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-06 18:04:52 +0200

    Add support for authentication to Ocelot

--- Micro.GatewaySolution/Program.cs
+++ Micro.GatewaySolution/Program.cs
@@ -1,10 +1,12 @@
-﻿using Ocelot.DependencyInjection;
+﻿using Micro.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.AddAppAuthentication();
 builder.Services.AddOcelot();
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot();
 app.Run();
