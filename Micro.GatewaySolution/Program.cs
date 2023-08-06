ed8a06bca94910445e8d28ee50ad9230d2343aa9
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-06 20:40:11 +0200

    Add jsonfile with ocelot endpoints for the apis

--- Micro.GatewaySolution/Program.cs
+++ Micro.GatewaySolution/Program.cs
@@ -5,9 +5,10 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
 
