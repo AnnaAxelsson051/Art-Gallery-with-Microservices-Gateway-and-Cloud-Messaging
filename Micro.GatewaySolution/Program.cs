5782ae621bdc7b7f649f4ac5e121f62b4c8f9ad1
Anna Axelsson <a.axelsson51@gmail.com>
2023-08-06 15:10:16 +0200

    Add gateway project and configure Ocelot

---
+++ Micro.GatewaySolution/Program.cs
@@ -0,0 +1,11 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+builder.Services.AddOcelot();
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+app.Run();
+
